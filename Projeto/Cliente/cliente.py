7ab1ee7cc11e87d242278dbc6bf3c19f8bd272b6
Jesue Lucas Diogo <jesuelucas@outlook.com>
2023-07-10 21:19:29 -0300

    Update cliente.py
    Inicio do codigo

--- Projeto/Cliente/cliente.py
+++ Projeto/Cliente/cliente.py
@@ -1,1 +1,13 @@
+#Data de criação: 10/07/2023 18:18
+#Ultima alteração: 10/07/2023 18:12
+#Autor: Jesue Lucas Diogo
+#Criação de um cliente para a atividade que selecione as opções e envia-las para o servidor
+#-----------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
+
+import socket, threading, sys, os
+
+servidor = '127.0.0.1'
+porta = 8500
+opcoes = 
 
